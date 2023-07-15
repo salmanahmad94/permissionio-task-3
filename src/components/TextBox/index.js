eb9ee1e412e189efcb05d46c631007ff6e7519a5
Salman Ahmad <salmanahmad94@gmail.com>
2023-07-15 17:56:10 +0500

    Style adjustments

--- src/components/TextBox/index.js
+++ src/components/TextBox/index.js
@@ -5,7 +5,7 @@ import classes from "./TextBox.module.css";
 const TextBox = () => {
   return (
     <div className={classes.TextBox}>
-      <p>
+      <p className={classes.Content}>
         Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
         accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab
         illo inventore veritatis et quasi architecto beatae vitae dicta sunt,
